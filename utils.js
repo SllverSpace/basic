708ce5ea3a1f3347ac20efabf7f28b64a0fa2637
Silver <rhyskoerber@gmail.com>
2023-12-19 17:02:07 +1030

    e

--- utils.js
+++ utils.js
@@ -79,16 +79,20 @@ class Utils {
     }
 
     addvl3(v1, v2) {
-        return [v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2]]
+        v1[0] += v2[0]; v1[1] += v2[1]; v1[2] += v2[2]
+        return v1
     }
     subvl3(v1, v2) {
-        return [v1[0]-v2[0], v1[1]-v2[1], v1[2]-v2[2]]
+        v1[0] -= v2[0]; v1[1] -= v2[1]; v1[2] -= v2[2]
+        return v1
     }
     mulvl3(v1, v2) {
-        return [v1[0]*v2[0], v1[1]*v2[1], v1[2]*v2[2]]
+        v1[0] *= v2[0]; v1[1] *= v2[1]; v1[2] *= v2[2]
+        return v1
     }
     divvl3(v1, v2) {
-        return [v1[0]/v2[0], v1[1]/v2[1], v1[2]/v2[2]]
+        v1[0] /= v2[0]; v1[1] /= v2[1]; v1[2] /= v2[2]
+        return v1
     }
 
     rotv2(vec, rot) {
