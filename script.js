9c20381b91f36e84b6fc8d3f51537ecf71771148
Silver <rhyskoerber@gmail.com>
2024-02-26 21:36:03 +1030

    e

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function update(timestamp) {
 
     dropdown.draw()
 
-    ui.text(10*su, 35*su, 25*su, "I have recently figured out that my website could look a lot more professional if i use html instead of js canvas, in english this just means that i don't like how pixelated everything is, and so i'm fixing it. This is going to take me a while as my previous ui system had 2000 lines of code, and i'll need to rewrite all of it for the new strat, and i'll also need to adapt all my recent js games into the new ui system because it would make them keep up to date with any bugfixes i make. Here, you can see my progress so far. https://basic2.silverspace.online - i can't be bothered making it a actual link sry lol", {wrap: 500*su})
-    ui.rect(515*su, 250*su, 10*su, 500*su, [255, 0, 0, 1])
+    // ui.text(10*su, 35*su, 25*su, "I have recently figured out that my website could look a lot more professional if i use html instead of js canvas, in english this just means that i don't like how pixelated everything is, and so i'm fixing it. This is going to take me a while as my previous ui system had 2000 lines of code, and i'll need to rewrite all of it for the new strat, and i'll also need to adapt all my recent js games into the new ui system because it would make them keep up to date with any bugfixes i make. Here, you can see my progress so far. https://basic2.silverspace.online - i can't be bothered making it a actual link sry lol", {wrap: 500*su})
+    // ui.rect(515*su, 250*su, 10*su, 500*su, [255, 0, 0, 1])
     // ui.rect(input.mouse.x, input.mouse.y, 50*su, 50*su, [255, 255, 255, 1])
 
     input.updateInput(true)
