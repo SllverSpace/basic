0b0638a28967a3fc7c65a434c6712394169524dd
Silver <rhyskoerber@gmail.com>
2024-05-01 20:19:45 +0930

    e

--- utils.js
+++ utils.js
@@ -141,9 +141,9 @@ class Utils {
     }
     rect2dc(x1, y1, w1, h1, x2, y2, w2, h2) {
         return (
-            x1+w1/2 > x2-w2/2,
-            x1-w1/2 < x2+w2/2,
-            y1+h1/2 > y2-h2/2,
+            x1+w1/2 > x2-w2/2 &&
+            x1-w1/2 < x2+w2/2 &&
+            y1+h1/2 > y2-h2/2 &&
             y1-h1/2 < y2+h2/2
         )
     }
